9ce4af710a7cb0df792b66a29186ec9a016d35bc
Danny L <wlou@ucdavis.edu>
2019-03-07 23:38:51 -0800

    updated pypoll script

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,68 +3,75 @@ import os
 import csv
 
 #set path of csv file
-budget_csv = os.path.join("..", "Resources", "election_data.csv")
+election_csv = os.path.join("..", "Resources", "election_data.csv")
 
 #create list to store csv data
-months = []
-profits = []
-averages = []
+total_votes = 0
 
-
+#dictionary to store candidate and vote count
+election = {}
 
 #open file, skip header, store data as rows in list
-with open(budget_csv, 'r') as csvfile:
+with open(election_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvfile)
-    for row in csvreader:
-        months.append(row[0])
-        profits.append(int(row[1]))
-        
-#calculate totals      
-    total_months = len(months)
-    total_revenue = sum(profits)
-
-#set variables to find greatest increase, decrease, and average change   
-    greatest_increase = profits[0]
-    greatest_decrease = profits[0]
-    previous_profit = 0    
- 
-#loop through list until conditionals are met   
-    for i in range(len(profits)):
-        if profits[i] >= greatest_increase:
-            greatest_increase = profits[i]
-            greatest_month = months[i]
-        if profits[i] <= greatest_decrease:
-            greatest_decrease = profits[i]
-            least_greatest_month = months[i]
-    for profit in profits:
-        if previous_profit != 0:
-            averages.append(int(profit - previous_profit))
-        previous_profit = profit
-    average = round(sum(averages)/total_months, 2)
     
-        
-    print("""
+    for row in csvreader:
+        total_votes += 1
+        if row[2] in election.keys():
+            election[row[2]] = election[row[2]] + 1
+        else:
+            election[row[2]] = 1
+ 
+#empty lists to store data
+candidates = []
+vote_count = []
+
+#fills empty list with key and value from dictionary
+for key, value in election.items():
+    candidates.append(key)
+    vote_count.append(value)
+
+#create vote percent list
+percent = []
+for i in vote_count:
+    percent.append(round(i/total_votes*100, 2))
+
+#zips name, vote count, and percent into one list to be printed
+print_list = list(zip(candidates, vote_count, percent))
+
+#store winner in list
+winner_list = []
+
+for name in print_list:
+    if max(vote_count) == name[1]:
+        winner_list.append(name[0])
+
+winner = winner_list[0]
+    
+print("""
 Election Results
 -------------------------------
           """)
-    print("Total Votes: ", )
-    print("-------------------------------")
-    print("Khan: ")
-    print("Correy: ")
-    print("Li: ")
-    print("O'Tooley: ")
-    print("-------------------------------")
-    print("Winner: ")
-    print("-------------------------------")
+print("Total Votes: ", total_votes)
+print("-------------------------------")
+for entry in print_list:
+    print(entry[0] + ": " + str(entry[2]) + "%  (" + str(entry[1]) + ")\n")
+print("-------------------------------")
+print("Winner: ", winner)
+print("-------------------------------")
 
 #create output path and file    
 output_file = os.path.join("..", "Output", "pypoll.txt")
 
 with open(output_file, "w", newline="") as datafile:
     writer = csv.writer(datafile)
-    datafile.write("Financial Analysis\n")
+    datafile.write("Election Results\n")
     datafile.write("-------------------------------\n")
-    datafile.write("Average Change: " + "$" + str(average) + "\n")
-    datafile.write("Greatest Increase in Profits: " + "$" + str(greatest_increase) + " " + greatest_month + "\n")
-    datafile.write("Greatest Decrease in Profits: " + "$" + str(greatest_decrease) + " " + least_greatest_month + "\n")
+    datafile.write("Total Votes: " + str(total_votes) + "\n")
+    datafile.write("-------------------------------" + "\n")
+    for entry in print_list:
+       datafile.write(entry[0] + ": " + str(entry[2]) + "%  (" + str(entry[1]) + ")\n")
+    datafile.write("-------------------------------" + "\n")
+    datafile.write("Winner: " + str(winner) + "\n")
+    datafile.write("-------------------------------" + "\n")
